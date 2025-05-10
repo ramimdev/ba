7a25cf87c9885b86d77202b34d914b43caa84a24
ramimdev <ourislamBD247@gmail.com>
2025-05-10 22:51:45 +0600

    Create u.js

---
+++ u.js
@@ -0,0 +1,7 @@
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&amp;c='+Math.random();f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TLQ8HF4X');</script>
+<!-- End Google Tag Manager -->
